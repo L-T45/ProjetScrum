609c7a8da8c5037c00dc7d37ac1e8081483b99fb
morgan <morgan.jouard@gmail.com>
2019-11-06 16:17:00 +0100

    1st commit

--- src/main/java/org/insset/server/RomanConverterServiceImpl.java
+++ src/main/java/org/insset/server/RomanConverterServiceImpl.java
@@ -19,7 +19,12 @@ public class RomanConverterServiceImpl extends RemoteServiceServlet implements
     @Override
     public String convertDateYears(String nbr) throws IllegalArgumentException {
         //Implement your code
-        return "XV/III/MX";
+        Integer date, month, years;
+        String[] str = nbr.split("/");
+        date = Integer.parseInt(str[0]);
+        month = Integer.parseInt(str[1]);
+        years = Integer.parseInt(str[2]);
+        return this.convertArabeToRoman(date) + "/" + this.convertArabeToRoman(month) + "/" + this.convertArabeToRoman(years);
     }
 
     @Override
@@ -31,7 +36,61 @@ public class RomanConverterServiceImpl extends RemoteServiceServlet implements
     @Override
     public String convertArabeToRoman(Integer nbr) throws IllegalArgumentException {
         //Implement your code
-        return new String("XVXX");
+    if (nbr < 1 || nbr > 2000)
+        return "valeur incorrect";
+    String s = "";
+    while (nbr >= 1000) {
+        s += "M";
+        nbr -= 1000;        }
+    while (nbr >= 900) {
+        s += "CM";
+        nbr -= 900;
+    }
+    while (nbr >= 500) {
+        s += "D";
+        nbr -= 500;
+    }
+    while (nbr >= 400) {
+        s += "CD";
+        nbr -= 400;
+    }
+    while (nbr >= 100) {
+        s += "C";
+        nbr -= 100;
+    }
+    while (nbr >= 90) {
+        s += "XC";
+        nbr -= 90;
+    }
+    while (nbr >= 50) {
+        s += "L";
+        nbr -= 50;
+    }
+    while (nbr >= 40) {
+        s += "XL";
+        nbr -= 40;
+    }
+    while (nbr >= 10) {
+        s += "X";
+        nbr -= 10;
+    }
+    while (nbr >= 9) {
+        s += "IX";
+        nbr -= 9;
+    }
+    while (nbr >= 5) {
+        s += "V";
+        nbr -= 5;
+    }
+    while (nbr >= 4) {
+        s += "IV";
+        nbr -= 4;
+    }
+    while (nbr >= 1) {
+        s += "I";
+        nbr -= 1;
+    }    
+    return s;
     }
 
 }
